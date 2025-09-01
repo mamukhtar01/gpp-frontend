964f7cc62d85f9581be018a89dd0997816cfd995
MUKHTAR MAHAMED <mamukhtar@iom.int>
2025-09-01 11:26:37 +0300

    Fix text color

--- components/layout-container.tsx
+++ components/layout-container.tsx
@@ -23,12 +23,12 @@ export default function LayoutContainer({ children }: Props) {
         />
       </Head>
       <body>
-        <div className="min-h-screen flex flex-col bg-gray-50">
+        <div className="min-h-screen flex flex-col bg-gray-50 text-black">
           <header className="bg-white border-b">
             <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
               <Link href="/" className="flex items-center space-x-2">
                 <Image src="/iom-logo.svg" alt="" width={100} height={47} />
-                <span className="ml-6 font-semibold text-lg">GPP Payments</span>
+                <span className="ml-6 font-semibold text-lg">Global Payment Platform</span>
               </Link>
               <MenuTop />
             </div>
