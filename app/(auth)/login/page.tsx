92a9df3c435f1d42992f59ca6cc5bd271e2f12ee
MUKHTAR MAHAMED <mamukhtar@iom.int>
2025-09-11 13:50:19 +0300

    Restyled Login Page bg image

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -35,7 +35,8 @@ export default async function LoginPage() {
         <Image
           src="/bg-vertical.png"
           alt="Global Payment Platform"
-          fill
+          width={600}
+          height={1000}
           className="object-cover"
           priority
         />
