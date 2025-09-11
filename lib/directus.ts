499b5d8c81a9aec46f408a51f77463b0548cab0a
MUKHTAR MAHAMED <mamukhtar@iom.int>
2025-09-11 16:20:51 +0300

    Added backend url directly to create client function

--- lib/directus.ts
+++ lib/directus.ts
@@ -1,13 +1,13 @@
 import  { authentication, createDirectus, rest } from '@directus/sdk';
 import {DbSchema} from './schema';
 
-const url = process.env.DIRECTUS_URL ?? process.env.NEXT_PUBLIC_DIRECTUS_URL
+// const url = process.env.DIRECTUS_URL ?? process.env.NEXT_PUBLIC_DIRECTUS_URL
 
-if (!url) {
-  throw new Error("DIRECTUS_URL or NEXT_PUBLIC_DIRECTUS_URL is not defined");
-}
+// if (!url) {
+//   throw new Error("DIRECTUS_URL or NEXT_PUBLIC_DIRECTUS_URL is not defined");
+// }
 
-const client = createDirectus<DbSchema>(url!)
+const client = createDirectus<DbSchema>("https://iom-ppo-directus-dev.iom.int")
   .with(rest())
   .with(authentication("cookie", { credentials: "include" }));
 
