3226fffda73f86f44b8b304ffa331a4b0f3810fb
MUKHTAR MAHAMED <mamukhtar@iom.int>
2025-09-30 08:34:51 +0300

    Refactor callback page

--- app/(auth)/callback/page.tsx
+++ app/(auth)/callback/page.tsx
@@ -1,10 +1,43 @@
-import React, { Suspense } from "react";
-import CallbackClient from "@/components/auth/callback-client";
+"use client";
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React  from "react";
+import client from "@/lib/directus";
+import { readMe } from "@directus/sdk";
+
 
 export default function AuthCallbackPage() {
-  return (
-    <Suspense fallback={<p>Signing you in...</p>}>
-      <CallbackClient />
-    </Suspense>
-  );
+const router = useRouter();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    const accessToken = params.get("access_token");
+// fetch user info
+    const fetchUserInfo = async () => {
+      try {
+        const response = await client.request(readMe());
+        console.log("User info:", response);
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+      }
+    };
+ 
+    if (accessToken) {
+      // Save token securely (better: httpOnly cookie via API route)
+      localStorage.setItem("directus_token", accessToken);
+
+      console.log("Access token saved:", accessToken);
+
+      // Redirect to home
+     // router.push("/home");
+    } else {
+      //router.push("/login?error=missing_token");
+    }
+    // We intentionally only run this once on mount; params is stable for this route.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    fetchUserInfo();
+  }, []);
+
+  return <p>Signing you in...</p>;
 }
--- components/auth/callback-client.tsx
+++ components/auth/callback-client.tsx
@@ -13,10 +13,12 @@ export default function CallbackClient() {
       // Save token securely (better: httpOnly cookie via API route)
       localStorage.setItem("directus_token", accessToken);
 
+      console.log("Access token saved:", accessToken);
+
       // Redirect to home
-      router.push("/home");
+     // router.push("/home");
     } else {
-      router.push("/login?error=missing_token");
+      //router.push("/login?error=missing_token");
     }
     // We intentionally only run this once on mount; params is stable for this route.
     // eslint-disable-next-line react-hooks/exhaustive-deps
