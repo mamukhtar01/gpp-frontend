5730b3321c74473b23cd073865e901582eeaa648
MUKHTAR MAHAMED <mamukhtar@iom.int>
2025-10-09 14:58:23 +0300

    Restyle payment status table

--- app/(main)/payments/status/page.tsx
+++ app/(main)/payments/status/page.tsx
@@ -1,9 +1,10 @@
 export const dynamic = "force-dynamic"; // update data on every request
 
 import { getPaymentsAction } from "@/app/server_actions";
-import { columns } from "./columns";
+
 import { DataTable } from "./table";
 import { TPayment } from "@/lib/schema";
+
 
 
 export default async function PaymentStatus() {
@@ -16,13 +17,12 @@ const payments =  await getPaymentsAction() as TPayment[] | null;
 if (!payments) {
     return <div>No payments found.</div>;
 }
-
 // ensure we pass an array to DataTable
 
   return (
     <div className="flex flex-col items-center justify-center p-4">
       <h1 className="text-2xl font-bold mb-4">Payment Status</h1>
-      <DataTable data={payments} columns={columns} />
+      <DataTable data={payments} />
     </div>
   );
 }
